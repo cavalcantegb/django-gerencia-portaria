6c8177b7cb9ab818e3dba56f136f4fd5027079c0
Guilherme Cavalcante <cavalcante.guilherme91@gmail.com>
2021-04-29 13:49:11 -0300

    Removing secret key. Secret keys from the project are not available and will not be pushed to the repo

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o5728s@+*&^$mmx)+d6ze)b_f0+ims6=x-#86vsl7__4ba2z*4'
+SECRET_KEY = '*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
