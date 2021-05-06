8c2edd2670df3f6815f868a768f9ab486c9b6102
Guilherme Cavalcante <cavalcante.guilherme91@gmail.com>
2021-05-06 07:16:19 -0300

    Adding template and static files.

--- config/settings.py
+++ config/settings.py
@@ -62,7 +62,7 @@ AUTH_USER_MODEL = "usuarios.Usuario"
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,3 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
