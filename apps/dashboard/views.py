b2584fa1a0ce2065e0208507487254d92b6ff8a4
Guilherme Cavalcante <cavalcante.guilherme91@gmail.com>
2021-05-11 21:02:10 -0300

    Adding total visitantes by month and order visitante by arriving time

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from apps.visitantes.models import Visitante
 
+from django.utils import timezone
+
 def index(request):
 
-    todos_visitantes = Visitante.objects.all()
+    todos_visitantes = Visitante.objects.order_by(
+        "-horario_chegada"
+    )
 
     visitantes_aguardando = todos_visitantes.filter(
         status="AGUARDANDO",
@@ -17,12 +21,19 @@ def index(request):
         status="FINALIZADO",
     )
 
+    hora_atual = timezone.now()
+
+    visitantes_mes = todos_visitantes.filter(
+        horario_chegada__month=hora_atual.month,
+    )
+
     context = {
         "nome_pagina" : "Início da Dashboard",
         "todos_visitantes" : todos_visitantes,
         "visitantes_aguardando": visitantes_aguardando.count(),
         "visitantes_em_visita": visitantes_em_visita.count(),
         "visitantes_finalizado": visitantes_finalizado.count(),
+        "visitantes_mes": visitantes_mes.count(),
     }
 
     return render(request, 'index.html', context)
